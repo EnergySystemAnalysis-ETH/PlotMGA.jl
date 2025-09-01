b56e6d05dbc854b64788389a17645e8445081067
Kacper Kozerski <kozerskikacper@gmail.com>
2025-09-01 10:52:02 +0200

    fixed the export file

--- src/PlotMGA.jl
+++ src/PlotMGA.jl
@@ -1,10 +1,10 @@
 module PlotMGA
     include("plot_mga_data.jl")
-    export get_param
+    export get_spine_parameter
     export ProfileType
     export plot_all_mga_profiles
     export plot_technology_interactions
     export plot_technology_effects
     export EnergyUnit, MWh, GWh
-    export process
+    export process_spine_data
 end
